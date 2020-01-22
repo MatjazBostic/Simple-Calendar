d71b555593e8a6e65ba5ff31fc50ad3ed09e6193
mbostic <bosticmatjaz@gmail.com>
2020-01-22 09:53:46 +0100

    update

--- src/HelperFunctions.java
+++ src/HelperFunctions.java
@@ -1,7 +1,9 @@
 public class HelperFunctions {
 
     public static void main(String[] args){
-        System.out.println(getDayOfWeek(25, 1, 2020));
+        for(int i = 1; i <= 12; i++ ) {
+            System.out.println(getDaysInMonth(i, 2020));
+        }
     }
 
     public static boolean isLeapYear(int year){
@@ -17,11 +19,17 @@ public class HelperFunctions {
         }
     }
 
-    public static int getDayOfWeek(int d, int m, int y){
+    public static int getDaysInMonth(int month, int year){
+        // source: http://www.dispersiondesign.com/articles/time/number_of_days_in_a_month
+        return (month == 2) ? (isLeapYear(year) ? 29 : 28) : 31 - (month - 1) % 7 % 2;
+    }
+
+    private static int[] t = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
+
+    public static int getDayOfWeek(int day, int month, int year){
         // source: https://en.wikipedia.org/wiki/Determination_of_the_day_of_the_week#Sakamoto's_methods
-        int[] t = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
-        y -= (m < 3) ? 1 : 0;
-        int dayOfWeek = ( y + y/4 - y/100 + y/400 + t[m-1] + d) % 7;
+        year -= (month < 3) ? 1 : 0;
+        int dayOfWeek = ( year + year/4 - year/100 + year/400 + t[month-1] + day) % 7;
         // fix, so that monday is the first day instead of sunday
         dayOfWeek--;
         if (dayOfWeek == -1) {
--- src/SimpleCalendar.java
+++ src/SimpleCalendar.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.text.AbstractDocument;
 import javax.swing.text.AttributeSet;
@@ -8,20 +9,16 @@ import javax.swing.text.DocumentFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
-import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SimpleCalendar extends JFrame {
 
-    JComboBox<String> monthComboBox;
-    JTextField yearTextField;
-    JLabel notificationLabel;
-    JTable calendarTable;
+    private JComboBox<String> monthComboBox;
+    private JTextField yearTextField;
+    private JTextField customDateTextField;
+    private JLabel notificationLabel;
+    private DefaultTableModel calendarTableModel;
 
     SimpleCalendar(){
 
@@ -29,6 +26,12 @@ public class SimpleCalendar extends JFrame {
 
         String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
         monthComboBox = new JComboBox<>(months);
+        // On month selected listener
+        monthComboBox.addActionListener (new ActionListener () {
+            public void actionPerformed(ActionEvent e) {
+                setupCalendar();
+            }
+        });
 
         yearTextField = new JTextField("2020", 4);
         yearTextField.setFont(new Font("SansSerif", Font.BOLD, 17));
@@ -59,6 +62,21 @@ public class SimpleCalendar extends JFrame {
             }
         });
 
+        customDateTextField = new JTextField("1.1.2020", 6);
+        customDateTextField.setFont(new Font("SansSerif", Font.BOLD, 17));
+        customDateTextField.setHorizontalAlignment(SwingConstants.RIGHT);
+        // Select all text on click
+        customDateTextField.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                SwingUtilities.invokeLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        customDateTextField.selectAll();
+                    }
+                });
+            }
+        });
+
         JButton confirmYearButton = new JButton("Confirm");
         confirmYearButton.setPreferredSize(new Dimension(80,25));
 
@@ -69,65 +87,147 @@ public class SimpleCalendar extends JFrame {
             }
         });
 
+        JButton confirmCustomDateButton = new JButton("Confirm");
+        confirmCustomDateButton.setPreferredSize(new Dimension(80,25));
+
+        confirmCustomDateButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+                String dateStr = customDateTextField.getText();
+
+                String[] parts = dateStr.split("\\.");
+                if(parts.length != 3){
+                    notificationLabel.setText("Error: Incorrect custom date format.");
+                    return;
+                }
+                int day;
+                int month;
+                int year;
+                try {
+                    day = Integer.parseInt(parts[0]);
+                    month = Integer.parseInt(parts[1]);
+                    year = Integer.parseInt(parts[2]);
+                } catch (NumberFormatException ex){
+                    ex.printStackTrace();
+                    notificationLabel.setText("Error: Incorrect custom date format.");
+                    return;
+                }
+                if(month < 1 || month > 12){
+                    notificationLabel.setText("Error: Incorrect custom date. Month out of range.");
+                    return;
+                }
+                int numOfDays = HelperFunctions.getDaysInMonth(month, year);
+                if(day < 1 || day > numOfDays){
+                    notificationLabel.setText("Error: Incorrect custom date. Day out of range.");
+                    return;
+                }
+
+                if(!notificationLabel.getText().isEmpty()){
+                    notificationLabel.setText("");
+                }
+                yearTextField.setText(parts[2]);
+                // Don't have to call setupCalendar(), because this selection calls it:
+                monthComboBox.setSelectedIndex(month-1);
+            }
+        });
+
         // Setup bottom notification label
         notificationLabel = new JLabel("", JLabel.CENTER);
         notificationLabel.setVerticalAlignment(JLabel.BOTTOM);
         notificationLabel.setForeground(Color.RED);
-        EmptyBorder border = new EmptyBorder(5, 15, 5, 15);
-        notificationLabel.setBorder(border);
+        notificationLabel.setBorder(new EmptyBorder(0, 0, 5, 0));
 
         // Setup calendar table
-
         Object[] columnNames = new Object[]{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
-        DefaultTableModel calendarTableModel = new DefaultTableModel(columnNames, 0);
-        calendarTable = new JTable(calendarTableModel);
-        calendarTableModel.addRow(new Object[]{1, 2, 3, 4, 5, 6, 7});
 
+        calendarTableModel = new DefaultTableModel(columnNames, 0);
+
+        JTable calendarTable = new JTable(calendarTableModel);
+        calendarTable.setDefaultEditor(Object.class, null);
+        calendarTable.setDefaultRenderer(Object.class, new CustomCellRenderer());
 
         JScrollPane calendarScrollPane = new JScrollPane(calendarTable);
-        calendarScrollPane.setBorder(border);
+        calendarScrollPane.setBorder(new EmptyBorder(0, 15, 0, 15));
 
+        // Setup view
         JPanel topPanel = new JPanel();
         topPanel.add(monthComboBox);
         topPanel.add(yearTextField);
         topPanel.add(confirmYearButton);
-        topPanel.setBorder(border);
+        topPanel.setBorder(new EmptyBorder(5, 0, 5, 0));
 
+        JPanel customDatePanel = new JPanel();
+        JLabel customDateLabel = new JLabel("Custom date:");
+        customDatePanel.add(customDateLabel);
+        customDatePanel.add(customDateTextField);
+        customDatePanel.add(confirmCustomDateButton);
+
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+        bottomPanel.add(customDatePanel, BorderLayout.NORTH);
+        bottomPanel.add(notificationLabel, BorderLayout.CENTER);
+        bottomPanel.setPreferredSize(new Dimension(500, 57));
 
         setLayout(new BorderLayout());
         add(topPanel, BorderLayout.NORTH);
-        add(notificationLabel, BorderLayout.SOUTH);
-
+        add(bottomPanel, BorderLayout.SOUTH);
         add(calendarScrollPane, BorderLayout.CENTER);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(550, 400);
+        setSize(550, 270);
+        setMinimumSize(new Dimension(300, 260));
         setVisible(true);
 
+        setupCalendar();
     }
-
-    private boolean yearStringEmpty;
 
     private void setupCalendar(){
 
         String yearStr = yearTextField.getText();
         if(yearStr.isEmpty()){
             notificationLabel.setText("Error: Year input field is empty");
-            yearStringEmpty = true;
             return;
-        } else if(yearStringEmpty) {
-            yearStringEmpty = false;
+        }
+        if(!notificationLabel.getText().isEmpty()) {
             notificationLabel.setText("");
         }
 
         int year = Integer.parseInt(yearStr);
         int month = monthComboBox.getSelectedIndex() + 1;
         System.out.println("Setting up table. Year = " + year + ", month = " + month);
+        calendarTableModel.setRowCount(0);
 
+        int counter = 1;
+        int startDayOfWeek = HelperFunctions.getDayOfWeek(counter, month, year);
+        int daysInMonth = HelperFunctions.getDaysInMonth(month, year);
+
+        for(int i = 0; counter <= daysInMonth ; i++) {
+            Object[] firstRow = new Object[7];
+            for (; (startDayOfWeek + counter <= 7 * (i + 1)) && (counter <= daysInMonth); counter++) {
+                firstRow[startDayOfWeek + counter - 1 - i * 7] = counter;
+            }
+            calendarTableModel.addRow(firstRow);
+        }
+    }
+
+    private static class CustomCellRenderer extends DefaultTableCellRenderer {
+        private static Color sundayColor = new Color(217, 152, 136);
+        @Override
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int col) {
+
+            JLabel l = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, col);
+            l.setOpaque(true);
+            if(col == 6) {
+                l.setBackground(sundayColor);
+            } else
+                l.setBackground(Color.WHITE);
+            return l;
+
+        }
     }
 
     public static void main(String[] args){
         new SimpleCalendar();
     }
-
 }
